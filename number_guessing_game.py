7f4994f1d641f2f920757b6f2fc4ab4f5920e633
Fatma Betül Koçaman <110344827+fatmabetulkocaman@users.noreply.github.com>
2023-01-05 19:33:31 +0300

    Add files via upload

---
+++ number_guessing_game.py
@@ -0,0 +1,61 @@
+import random
+import time
+
+print(
+    """
+
+***************************
+NUMBER PREDICTION GAME
+------------------
+Rules of the Game:
+1.estimation range 1-75
+2.Press '0' to exit the game.
+
+***************************
+
+"""
+)
+
+
+random_ = random.randint(1,75)
+
+counter = 0
+
+while True:
+
+    counter += 1
+    number = int(input("Enter your guess:"))
+
+    if number == 0:
+        print("Exiting the game...")
+        time.sleep(7)
+        print("Exited the game!")
+        break
+
+    elif number < random_:
+        print("Being checking...")
+        time.sleep(3)
+        print("Enter a larger number!")
+        continue
+
+    elif number > random_:
+        print("Being checking...")
+        time.sleep(3)
+        print("Enter a smaller number!")
+        continue
+
+    elif number == random_:
+        print("Being checking...")
+        time.sleep(3)
+        print("Congratulations,your guess is correct!!")
+        print("Randomly selected number: {}".format(random_))
+        print("Number of guesses: {}".format(counter))
+        time.sleep(3)
+        print("Exiting the game...")
+        time.sleep(7)
+        print("Exited the game!")
+        break
+
+    else:
+        print("Incorrect data entry!!!")
+        continue
